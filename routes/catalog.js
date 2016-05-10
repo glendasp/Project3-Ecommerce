95b08058e3d3cc582458d6e635f0f0bb7f8e861a
glendex <glendasp@gmail.com>
2016-05-10 16:04:44 -0500

    Templates for products

--- routes/catalog.js
+++ routes/catalog.js
@@ -60,6 +60,36 @@ router.get("/search/:item_id", function(req, res) {
 });
 
 
+//routes - this is for the home page
+app.get('/', function(req, res) {
+
+    // Make query for unique colors, to populate the dropdown box
+    db.collection('products').distinct('color', function (err, colorDocs) {
+        if (err) {
+            return res.sendStatus(500);
+        }
+
+        //If selecting a color and clicked the colorButton, this has a value
+        if (req.query.colorButton) {
+            var color = req.query.colorDropDown;    //What color was selected?
+            //Get all of the flowers of the desired color.
+            db.collection('products').find({'color': color}).toArray(function (err, flowerDocs) {
+                if (err) {
+                    return res.sendStatus(500);
+                }
+                //Optional - turn 'red' into 'Red' for display
+                var displayColor = color.slice(0, 1).toUpperCase() + color.slice(1, color.length);
+                //return res.render statement inside a callback to prevent further processing of response
+
+                console.log(err);
+                //console.log('count?' + count);
+
+                //return res.render('productdetail',
+                //    {'flowers': flowerDocs, 'currentColor': displayColor, 'flowerColors': colorDocs});
+            });
+        }
+    });
+});
 
 
 module.exports = router;
